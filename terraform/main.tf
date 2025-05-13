56715f4635a00af4baec33cd28a77fa1920649f9
Tristan Nono <ricemaking69@gmail.com>
2025-05-13 13:59:03 -0500

    thing

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "example" {
+  name     = "${var.project_id}-example-bucket"
+  location = var.region
+  force_destroy = true
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  type        = string
+  description = "steady-copilot-459615-f4"
+}
+
+variable "region" {
+  type        = string
+  default     = "us-central1"
+  description = "us-central1"
+}
