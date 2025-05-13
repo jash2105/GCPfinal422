0c1577354e26f00f97713166f14ff295bfd585f4
Tristan Nono <ricemaking69@gmail.com>
2025-05-13 14:02:37 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,43 @@ resource "google_storage_bucket" "example" {
   location = var.region
   force_destroy = true
 }
+
+resource "google_compute_network" "custom_vpc" {
+  name                    = "custom-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "custom_subnet" {
+  name          = "custom-subnet"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.custom_vpc.id
+}
+
+resource "google_compute_firewall" "allow-http" {
+  name    = "allow-http"
+  network = google_compute_network.custom_vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  direction = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
+
+resource "google_compute_firewall" "allow-https" {
+  name    = "allow-https"
+  network = google_compute_network.custom_vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  direction = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["https-server"]
+}
