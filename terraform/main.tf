6333f5f0e7b7bdd40bf5a975d15a8679ebb88dcf
Tristan Nono <ricemaking69@gmail.com>
2025-05-14 11:09:07 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,6 +64,8 @@ resource "google_compute_instance" "flask-vm" {
   machine_type = "e2-medium"
   zone         = "us-central1-c"
 
+  tags = ["http-server"]
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
